35561f17b47dafa7fa8fd23c99442c0ef7e0c808
janellebaguley <nellie.baguley@gmail.com>
2021-01-02 07:58:33 -0700

    updates

--- server/controller.js
+++ server/controller.js
@@ -1,33 +1,58 @@
 const axios = require('axios');
 
-let tops = [],
-    bottoms = [],
-    shoes = [],
-    hats = [],
-    id = 1;
+const clothes = {
+    tops= [
+        {id: 1, tops: 'blouse', size: 'S'},
+        {id: 2, tops: 'button-up', size: 'S'}, 
+        {id: 3, tops: 't-shirt', size: 'S'}, 
+        {id: 4, tops: 'tank-top', size: 'S'}, 
+        {id: 5, tops: 'sweater', size: 'S'},
+        {id: 6, tops: 'turtleneck', size: 'S'}],
+    bottoms = [
+        {id: 1, bottoms: 'jeans', size: 'S'},
+        {id: 2, bottoms: 'shorts', size: 'S'},
+        {id: 3, bottoms: 'leggings', size: 'S'},
+        {id: 4, bottoms: 'skirt', size: 'S'},
+        {id: 5, bottoms: 'skorts', size: 'S'},
+        {id: 6, bottoms: 'capris', size: 'S'}],
+    shoes = [
+        {id: 1, shoes: 'tennis-shoes', size: 6},
+        {id: 2, shoes: 'high heels', size: 6},
+        {id: 3, shoes: 'flip-flops', size: 6},
+        {id: 4, shoes: 'boots', size: 6},
+        {id: 5, shoes: 'flats', size: 6},
+        {id: 6, shoes: 'wedges', size: 6}],
+    hats = [
+        {id: 1, hats: 'baseball cap', size: 'S'},
+        {id: 2, hats: 'fedora', size: 'S'},
+        {id: 3, hats: 'cowboy hat', size: 'S'},
+        {id: 4, hats: 'newsboy cap', size: 'S'},
+        {id: 5, hats: 'beanie', size: 'S'},
+        {id: 6, hats: 'bucket hat', size: 'S'}
+    ]};
+let id = 1;
 
 module.exports = {
-    addItem: (req,res) => {
-        // res.status(200).send(caughtPokemon);
+    getClothes: (req,res) => {
+        res.status(200).send(clothes);
     },
-    postItem: (req,res) => {
-        // const {pokemon} = req.body;
-        // pokemon.id = id;
-        // id++;
-        // caughtPokemon.push(pokemon);
-        // res.status(200).send(caughtPokemon);
+    addClothes: (req,res) => {
+        const {clothes} = req.body;
+        clothes.id = id;
+        id++;
+        clothes.push(clothes);
+        res.status(200).send(clothes);
     },
     editSize: (req,res) => {
-        // const {id} = req.params,
-        //       {name} = req.body;
-        // const pokemon = caughtPokemon.find(element => element.id === +id);
-        // pokemon.name = name;
-        // res.status(200).send(caughtPokemon);
+        const {id} = req.params,
+        const clothes = clothes.find(element => element.id === +id);
+        clothes.size = size;
+        res.status(200).send(clothes);
     },
     removeFromCart: (req,res) => {
-        // const {id} = req.params;
-        // const index = caughtPokemon.findIndex(element => element.id === +id);
-        // caughtPokemon.splice(index, 1);
-        // res.status(200).send(caughtPokemon)
+        const {id} = req.params;
+        let clothesId = clothes.findIndex(element => element.id === +id);
+        clothes.splice(index, 1);
+        res.status(200).send(clothes)
     }
 }
