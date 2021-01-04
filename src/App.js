6e532c27af972504e73f5e493b5e200cd13b940c
janellebaguley <nellie.baguley@gmail.com>
2021-01-04 16:07:11 -0700

    updates

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import React, {Component} from 'react';
-import axios from 'axios';
 import Header from './Components/Header';
-import Cart from '/Components/Cart';
+import Cart from './Components/Cart';
+import axios from 'axios';
 import './App.css';
 
-
-class App extends Component() {
+class App extends Component {
   constructor(props){
-    super(props)
+    super(props);
     this.state = {
-    clothesInCart: []
+    clothes: {
+      tops: [],
+      bottoms: [],
+      shoes: [],
+      hats: [],
+      i: 0
+    }
   }
-  this.clothesInCart = this.addClothes.bind(this);
+  this.addClothes = this.addClothes.bind(this);
+  this.getClothes = this.getClothes.bind(this);
 } 
 componentDidMount(){
   axios.get('/api/clothes')
   .then(res => {
-    this.setState({clothesInCart: res.data})
+    this.setState({clothes: res.data})
   })
   .catch(err => console.log(err))
 }
 
-addClothes = () =>
+getClothes = () => {
+  axios.get('/api/clothes')
+  .then(res => {
+    this.setState({clothes: res.data})
+  })
+  .catch(err => console.log(err))
+}
+
+addClothes =() => {
   axios.post('/api/clothes', {tops: this.state.tops, bottoms: this.state.bottoms, shoes: this.state.shoes, hats: this.state.hats})
   .then(res => {
     this.setState({clothes: res.data, 
@@ -29,18 +43,18 @@ addClothes = () =>
       bottoms: [],
       shoes: [],
       hats: []})
-    })
+    })   
     .catch(err => console.log(err))
-  };
-  editSize= (id, newSize) => {
-    let body = {size: newSize};
-    axios.put(`/api/clothes/${id}`, body)
-    .then(res => {
-      this.setState({clothesInCart: res.data})
-    })
-    .catch(err => console.log(err))
-  };
-  removeFromCart= (id) => {
+}
+editSize= (id, newSize) => {
+  let body = {size: newSize};
+  axios.put(`/api/clothes/${id}`, body)
+  .then(res => {
+    this.setState({clothesInCart: res.data})
+  })
+  .catch(err => console.log(err))
+};
+removeFromCart= (id) => {
     axios.delete(`/api/clothes/${id}`)
     .then(res => {
       this.setState({clothesInCart: res.data})
@@ -49,10 +63,16 @@ addClothes = () =>
   };
 
   render(){
+    const clothes = this.state.clothes;
+    const tops = this.state.clothes.tops;
+    const bottoms = this.state.clothes.bottoms;
+    const shoes = this.state.clothes.shoes;
+    const hats = this.state.clothes.hats
   return (
     <div className="App">
       <Header/>
-      <Cart/>
+      <h2>Cart</h2>
+      <Cart/>     
     </div>
    );
   }
--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,38 +1,29 @@
 import React, {Component} from 'react';
-import Outfit from './Outfit';
 
-class Cart extends Component() {
+class Cart extends Component {
     constructor(props){
       super(props)
       this.state ={
-        size: this.props.clothes.size
-      }
+        sizeInput: ''
   }
-handleInputs = (e) => {
-    this.setState({
-        [e.target.name]: e.target.value
-    })
+ }
+handleInput = (val) => {
+    this.setState({sizeInput: val})
 }
-
+handleEdit = (id) => {
+  this.props.sizeFn(id, this.state.sizeInput)
+}
     render(){
-      const {size} = this.state;
-      const {clothes} = this.props;
     return (
       <div>
         <section>
-          <input value = {size} name = 'size' onChange={e => this.handleInputs(e)}/>
+          {/* <input value = {sizeFn} name = 'size' onChange={e => this.handleInput(e)}/> */}
           <button onClick ={this.updateSize}>Update</button>
-        </section>
-        <section>
-          <h4>{clothes.tops}</h4>
-          <h4>{clothes.bottoms}</h4>
-          <h4>{clothes.shoes}</h4>
-          <h4>{clothes.hats}</h4>
           <button onClick ={this.removeFromCart}>Delete</button> 
         </section>
       </div>
     );
-    }
   }
 }
+
 export default Cart;
